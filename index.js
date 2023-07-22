4cb11baff1a708987065ada2330321b898525a09
Kamil Markow <kamilmarkow@gmail.com>
2023-07-22 14:38:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('Hello from tutorial!');
+
+
+// TODO: Ideally this would be working
+
+// TODO(Rugvip): Not sure why this works, investigate
+
+// @todo: This worked last Monday /Rugvip
+
+// FIXME Nobody knows why this is here
